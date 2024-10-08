fd27514300967b9f4d023b087e5ef440b0f24529
VyBui13 <vybuoi0904@gmail.com>
2024-10-08 13:04:40 +0700

    Display books in database to UI

--- frontend/src/pages/BookList.jsx
+++ frontend/src/pages/BookList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-
+import '../styles/BookList.css';
 
 function BookList() {
     const [books, setBooks] = useState([]);
@@ -10,6 +10,7 @@ function BookList() {
         fetch('http://localhost:5000/books')
             .then(response => response.json())
             .then(data => {
+                console.log(data);
                 setBooks(data);
             });
     }, []);
@@ -18,7 +19,45 @@ function BookList() {
         <>
             <div className="booklist-container">
                 <div className="booklist">
+                    <div className="booklist__title">
+                        Book List
+                    </div>
+                    <div className="booklist__content">
+                        <div className="booklist__field">
+                            <div className="booklist__attribute">
+                                Name
+                            </div>
+                            <div className="booklist__attribute">
+                                Kind
+                            </div>
+                            <div className="booklist__attribute">
+                                Author
+                            </div>
+                            <div className="booklist__attribute">
+                                {/* PresentAmount */}
+                                Amount
+                            </div>
+                        </div>
 
+                        <div className="booklist__items">
+                            {books.map((book) => (
+                                <div className="booklist__item">
+                                    <div className="booklist__attribute">
+                                        {book.Name}
+                                    </div>
+                                    <div className="booklist__attribute">
+                                        {book.Kind}
+                                    </div>
+                                    <div className="booklist__attribute">
+                                        {book.Author}
+                                    </div>
+                                    <div className="booklist__attribute">
+                                        {book.PresentAmount}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
