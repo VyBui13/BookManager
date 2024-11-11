1a6f356ae7f16ba46b33636bc787deb42b81a857
VyBui13 <vybuoi0904@gmail.com>
2024-11-11 22:40:17 +0700

    Change layout, set responsive for booklist, css for booklist

--- frontend/src/pages/BookList.jsx
+++ frontend/src/pages/BookList.jsx
@@ -7,12 +7,22 @@ import { faBook, faMagnifyingGlass, faUpLong, faDownLong } from '@fortawesome/fr
 
 function BookList() {
     const [books, setBooks] = useState([]);
+    const [kinds, setKinds] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:5000/books')
             .then(response => response.json())
             .then(data => {
                 setBooks(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        fetch('http://localhost:5000/books/kinds')
+            .then(response => response.json())
+            .then(data => {
+                setKinds(data);
             })
             .catch((error) => {
                 console.log(error);
@@ -27,53 +37,69 @@ function BookList() {
                     <h1>Booklist</h1>
                 </div>
 
-                {/* <div className="booklist__search">
-                    <button className="booklist__search-item">
-                        <div className="booklist__search-icon">
-                            <FontAwesomeIcon icon={faBook} className='icon__book' />
-                        </div>
-                        <div className="booklist__search-title">
-                            <h1>Search</h1>
-                            <p>by name</p>
-                        </div>
-                    </button>
-
-                    <button className="booklist__search-item">
-                        <div className="booklist__search-icon">
-                            <FontAwesomeIcon icon={faBook} className='icon__book' />
-                        </div>
-                        <div className="booklist__search-title">
-                            <h1>Search</h1>
-                            <p>by kind</p>
-                        </div>
-                    </button>
-
-                    <button className="booklist__search-item">
-                        <div className="booklist__search-icon">
-                            <FontAwesomeIcon icon={faBook} className='icon__book' />
-                        </div>
-                        <div className="booklist__search-title">
-                            <h1>Search</h1>
-                            <p>by author</p>
-                        </div>
-                    </button>
-
-                    <button className="booklist__search-item">
-                        <div className="booklist__search-icon">
-                            <FontAwesomeIcon icon={faBook} className='icon__book' />
-                        </div>
-                        <div className="booklist__search-title">
-                            <h1>Sort</h1>
-                            <p>by price</p>
-                        </div>
-                    </button>
-
-                </div> */}
                 <div className="booklist__body">
+                    <div className="booklist__search">
+                        <div className="booklist__search-header">
+                            <h2>Search</h2>
+                        </div>
+                        <div className="booklist__searchbar">
+
+                            <input type="text" placeholder="Search by name" />
+                            <button>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} className='icon__search' />
+                            </button>
+                        </div>
+
+                        <div className="booklist__search-header">
+                            <h2>Kind</h2>
+                        </div>
+                        <div className="booklist__filterkind">
+                            {kinds.map(kind => (
+                                <div className="booklist__filterkind-item" key={kind}>
+                                    <input type="checkbox" id={kind} />
+                                    <label htmlFor={kind}>{kind}</label>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="booklist__search-header">
+                            <h2>Sort</h2>
+                        </div>
+
+                        <div className="booklist__sort">
+                            <button className="booklist__sort-item">
+                                <FontAwesomeIcon icon={faUpLong} className='icon__sort' />
+                                <div className="booklist__sort-nameitem">
+                                    Price
+                                </div>
+                            </button>
+
+                            <button className="booklist__sort-item">
+                                <FontAwesomeIcon icon={faDownLong} className='icon__sort' />
+                                <div className="booklist__sort-nameitem">
+                                    Price
+                                </div>
+                            </button>
+
+                            <button className="booklist__sort-item">
+                                <FontAwesomeIcon icon={faUpLong} className='icon__sort' />
+                                <div className="booklist__sort-nameitem">
+                                    Name
+                                </div>
+                            </button>
+
+                            <button className="booklist__sort-item">
+                                <FontAwesomeIcon icon={faDownLong} className='icon__sort' />
+                                <div className="booklist__sort-nameitem">
+                                    Name
+                                </div>
+                            </button>
+                        </div>
+                    </div>
                     <div className="booklist">
                         {
                             books.map(book => (
-                                <div className="booklist__item">
+                                <div className="booklist__item" key={book._id}>
                                     <div className="booklist__icon">
                                         <FontAwesomeIcon icon={faBook} className='icon__book' />
                                     </div>
@@ -114,77 +140,6 @@ function BookList() {
 
                     </div>
 
-                    <div className="booklist__search">
-                        <div className="booklist__search-header">
-                            <h2>Search</h2>
-                        </div>
-                        <div className="booklist__searchbar">
-
-                            <input type="text" placeholder="Search by name" />
-                            <button>
-                                <FontAwesomeIcon icon={faMagnifyingGlass} className='icon__search' />
-                            </button>
-                        </div>
-
-                        <div className="booklist__search-header">
-                            <h2>Kind</h2>
-                        </div>
-                        <div className="booklist__filterkind">
-                            <div className="booklist__filterkind-item">
-                                <input type="checkbox" id="1" />
-                                <label htmlFor="1">Novel</label>
-                            </div>
-
-                            <div className="booklist__filterkind-item">
-                                <input type="checkbox" id="2" />
-                                <label htmlFor="2">Fiction</label>
-                            </div>
-
-                            <div className="booklist__filterkind-item">
-                                <input type="checkbox" id="3" />
-                                <label htmlFor="3">Biography</label>
-                            </div>
-
-                            <div className="booklist__filterkind-item">
-                                <input type="checkbox" id="4" />
-                                <label htmlFor="4">Autobiography</label>
-                            </div>
-                        </div>
-
-                        <div className="booklist__search-header">
-                            <h2>Sort</h2>
-                        </div>
-
-                        <div className="booklist__sort">
-                            <button className="booklist__sort-item">
-                                <FontAwesomeIcon icon={faUpLong} className='icon__sort' />
-                                <div className="booklist__sort-nameitem">
-                                    Price
-                                </div>
-                            </button>
-
-                            <button className="booklist__sort-item">
-                                <FontAwesomeIcon icon={faDownLong} className='icon__sort' />
-                                <div className="booklist__sort-nameitem">
-                                    Price
-                                </div>
-                            </button>
-
-                            <button className="booklist__sort-item">
-                                <FontAwesomeIcon icon={faUpLong} className='icon__sort' />
-                                <div className="booklist__sort-nameitem">
-                                    Name
-                                </div>
-                            </button>
-
-                            <button className="booklist__sort-item">
-                                <FontAwesomeIcon icon={faDownLong} className='icon__sort' />
-                                <div className="booklist__sort-nameitem">
-                                    Name
-                                </div>
-                            </button>
-                        </div>
-                    </div>
 
                 </div>
             </div >
