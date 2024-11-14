fbd7f366d2fa62c07e4431ba234881dbb7aedb18
VyBui13 <vybuoi0904@gmail.com>
2024-11-14 22:57:09 +0700

    Reponsive for setprice page again

--- frontend/src/components/Form/Setprice.jsx
+++ frontend/src/components/Form/Setprice.jsx
@@ -5,6 +5,7 @@ import BookSelected from '../BookSelected';
 import '../../styles/List.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
+import NothingDisplay from '../NothingDiplay';
 
 function setPrice() {
     const [books, setBooks] = useState([]);
@@ -28,48 +29,47 @@ function setPrice() {
                         <FontAwesomeIcon icon={faBookOpen} className="icon__header" />
                     </div>
                     <div className="list__body">
-
-                        <div className="list__content">
-
-                            <div className="list__field">
-                                <div className="list__attribute">
-                                    Name
-                                </div>
-                                <div className="list__attribute">
-                                    Kind
-                                </div>
-                                <div className="list__attribute">
-                                    Author
-                                </div>
-                                <div className="list__attribute">
-                                    Price
-                                </div>
-                            </div>
-
-                            <div className="list__items">
-                                {books.map((book) => (
-                                    <div className="list__item" key={book._id}>
-                                        <div className="list__attribute">
-                                            {book.bookName}
-                                        </div>
-                                        <div className="list__attribute">
-                                            {book.bookKind}
-                                        </div>
-                                        <div className="list__attribute">
-                                            {book.bookAuthor}
-                                        </div>
-                                        <div className="list__attribute">
-                                            <button style={book.bookPrice === 0 ? { backgroundColor: 'red' } : { backgroundColor: '#77f740' }}
-                                                onClick={(e) => {
-                                                    const updateBook = books.find(b => b._id === book._id);
-                                                    setBookPrice(updateBook);
-                                                }}
-                                            >Edit</button>
-                                        </div>
+                        {books.length === 0 ? <NothingDisplay /> :
+                            <div className="list__content">
+                                <div className="list__field">
+                                    <div className="list__attribute">
+                                        Name
                                     </div>
-                                ))}
-                            </div>
-                        </div>
+                                    <div className="list__attribute">
+                                        Kind
+                                    </div>
+                                    <div className="list__attribute">
+                                        Author
+                                    </div>
+                                    <div className="list__attribute">
+                                        Price
+                                    </div>
+                                </div>
+
+                                <div className="list__items">
+                                    {books.map((book) => (
+                                        <div className="list__item" key={book._id}>
+                                            <div className="list__attribute">
+                                                {book.bookName}
+                                            </div>
+                                            <div className="list__attribute">
+                                                {book.bookKind}
+                                            </div>
+                                            <div className="list__attribute">
+                                                {book.bookAuthor}
+                                            </div>
+                                            <div className="list__attribute">
+                                                <button style={book.bookPrice === 0 ? { backgroundColor: 'red' } : { backgroundColor: '#77f740' }}
+                                                    onClick={(e) => {
+                                                        const updateBook = books.find(b => b._id === book._id);
+                                                        setBookPrice(updateBook);
+                                                    }}
+                                                >Edit</button>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>}
                     </div>
                 </div>
             </div>
